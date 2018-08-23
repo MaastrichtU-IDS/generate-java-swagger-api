8f04364fe2e37a247fb5d65196aee08309f625bf
vemonet <vincent.emonet@gmail.com>
2018-08-23 17:16:16 +0200

    now properly display model and able to get back xml response

--- src/main/java/com/example/SparqlResource.java
+++ src/main/java/com/example/SparqlResource.java
@@ -25,21 +25,46 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
+import java.util.Arrays;
+
 import static com.github.tminglei.bind.Mappings.longv;
+import static com.github.tminglei.bind.Mappings.*;
 import static com.github.tminglei.swagger.SwaggerContext.param;
 import static com.github.tminglei.swagger.SwaggerContext.response;
 import static com.github.tminglei.swagger.SwaggerContext.sharing;
+import static com.github.tminglei.swagger.SwaggerContext.*;
+import static com.github.tminglei.bind.Simple.*;
+import static com.github.tminglei.bind.Mappings.*;
+import static com.github.tminglei.bind.Constraints.*;
+import static com.github.tminglei.bind.Processors.*;
 
 @Path("/sparql")
 @Produces({"application/json", "application/xml"})
 public class SparqlResource {
+
+
+    /**
+     * To describe the object returned by the API
+     */
+    static Mapping<?> sparqlMapping = $(mapping(
+            field("id", $(longv()).desc("pet id").example(3).$$),
+            field("name", $(text(required())).desc("pet name").$$),
+            field("category", $(mapping(
+                    field("id", longv(required())),
+                    field("name", text(required()))
+            )).desc("category belonged to").$$),
+            field("photoUrls", $(list(text())).desc("pet's photo urls").example(Arrays.asList("http://example.com/photo1")).$$),
+            field("tags", $(list(text())).desc("tags for the pet").example(Arrays.asList("tag1", "tag2")).$$)
+    )).refName("SparqlRef").desc("Sparql info").$$;
+
+
     static SharingHolder sharing = sharing().pathPrefix("/sparql").tag("sparql");
 
     static {
         sharing.operation(HttpMethod.GET, "/:query<[0-9]+>")
                 .summary("A valid SPARQL query.")
                 .parameter(param(longv()).in("path").name("query").example(1l))
-                .response(200, response().description("sparql found!"))
+                .response(200, response(sparqlMapping))
                 .response(404, response().description("sparql not found"))
         ;
     }
