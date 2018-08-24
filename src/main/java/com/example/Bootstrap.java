6f12655561c4307364e7aaa3ef3c29aa5a1beb8f
vemonet <vincent.emonet@gmail.com>
2018-08-24 02:14:45 +0200

    working with only bootstrap file

--- src/main/java/com/example/Bootstrap.java
+++ src/main/java/com/example/Bootstrap.java
@@ -21,41 +21,49 @@ import io.swagger.models.HttpMethod;
 import io.swagger.models.auth.In;
 
 import javax.servlet.http.HttpServlet;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+import java.util.Arrays;
 
 import static com.github.tminglei.bind.Constraints.required;
-import static com.github.tminglei.bind.Mappings.longv;
-import static com.github.tminglei.bind.Mappings.text;
+import static com.github.tminglei.bind.Mappings.*;
 import static com.github.tminglei.bind.Simple.*;
 import static com.github.tminglei.swagger.SwaggerContext.*;
 
+@Path("/sparql")
+@Produces({"application/json", "application/xml"})
 public class Bootstrap extends HttpServlet {
-  private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-  static {  // for swagger
-    swagger().info(info()
-              .title("Bio2RDF")
-              .description("Bio2RDF is an open source project to generate and provide linked data for the life sciences.")
-              .termsOfService("https://github.com/bio2rdf/bio2rdf-scripts/wiki/Terms-of-use")
-              .contact(contact().name("Michel Dumontier").email("michel.dumontier@gmail.com"))
-              .license(license().name("MIT License")
-                              .url("https://github.com/bio2rdf/bio2rdf-scripts/blob/master/MIT-LICENSE.txt")
-              )
-      ).host("localhost:8002")
-      .basePath("/api")
-      .consumes("application/json").consumes("application/xml")
-      .produces("application/json").produces("application/xml")
-      .securityDefinition("api_key", apiKeyAuth("api_key", In.HEADER))
-      .securityDefinition("petstore_auth", oAuth2()
-                      .implicit("http://petstore.swagger.io/api/oauth/dialog")
-                      .scope("read:pets", "read your pets")
-                      .scope("write:pets", "modify pets in your account")
-      ).tag(tag("pet").description("Everything about your Pets")
-                      .externalDocs(externalDocs().description("Find out more").url("http://swagger.io"))
-      ).tag(tag("store").description("Access to Petstore orders")
-      ).tag(tag("user").description("Operations about user")
-              .externalDocs(externalDocs().description("Find out more about our store").url("http://swagger.io"))
-      );
-  }
+
+    static {  // for swagger
+        swagger().info(info()
+                .title("Bio2RDF")
+                .description("Bio2RDF is an open source project to generate and provide linked data for the life sciences.")
+                .termsOfService("https://github.com/bio2rdf/bio2rdf-scripts/wiki/Terms-of-use")
+                .contact(contact().name("Michel Dumontier").email("michel.dumontier@gmail.com"))
+                .license(license().name("MIT License")
+                        .url("https://github.com/bio2rdf/bio2rdf-scripts/blob/master/MIT-LICENSE.txt")
+                )
+        ).host("localhost:8002")
+                .basePath("/api")
+                .consumes("application/json").consumes("application/xml")
+                .produces("application/json").produces("application/xml")
+                .securityDefinition("api_key", apiKeyAuth("api_key", In.HEADER))
+                .securityDefinition("petstore_auth", oAuth2()
+                        .implicit("http://petstore.swagger.io/api/oauth/dialog")
+                        .scope("read:pets", "read your pets")
+                        .scope("write:pets", "modify pets in your account")
+                ).tag(tag("pet").description("Everything about your Pets")
+                .externalDocs(externalDocs().description("Find out more").url("http://swagger.io"))
+        ).tag(tag("store").description("Access to Petstore orders")
+        ).tag(tag("user").description("Operations about user")
+                .externalDocs(externalDocs().description("Find out more about our store").url("http://swagger.io"))
+        );
+    }
 
 
     static Mapping<?> getItemMapping = $(mapping(
@@ -66,16 +74,48 @@ public class Bootstrap extends HttpServlet {
 
 
 
-    static SharingHolder sharing = sharing().pathPrefix("/getItem").tag("getItem");
+    static SharingHolder sharing2 = sharing().pathPrefix("/getItem").tag("getItem");
 
     static {
-        sharing.operation(HttpMethod.GET, "/{prefix}:{identifier}")
+        sharing2.operation(HttpMethod.GET, "/{prefix}:{identifier}")
                 .summary("A item query.")
                 .parameter(param(longv()).in("path").name("prefix").example(1l))
                 .parameter(param(longv()).in("path").name("identifier").example(1l))
                 .response(200, response(getItemMapping))
                 .response(404, response().description("item not found"))
         ;
+    }
+
+
+    /**
+     * To describe the object returned by the API
+     */
+    static Mapping<?> sparqlMapping = $(mapping(
+            field("id", $(longv()).desc("pet id").example(3).$$),
+            field("name", $(text(required())).desc("pet name").$$),
+            field("category", $(mapping(
+                    field("id", longv(required())),
+                    field("name", text(required()))
+            )).desc("category belonged to").$$),
+            field("photoUrls", $(list(text())).desc("pet's photo urls").example(Arrays.asList("http://example.com/photo1")).$$),
+            field("tags", $(list(text())).desc("tags for the pet").example(Arrays.asList("tag1", "tag2")).$$)
+    )).refName("SparqlRef").desc("Sparql info").$$;
+
+
+    static SharingHolder sharing = sharing().pathPrefix("/sparql").tag("sparql");
+
+    static {
+        sharing.operation(HttpMethod.GET, "/:query<[0-9]+>")
+                .summary("A valid SPARQL query.")
+                .parameter(param(longv()).in("path").name("query").example(1l))
+                .response(200, response(sparqlMapping))
+                .response(404, response().description("sparql not found"))
+        ;
+    }
+    @GET
+    @Path("/{query}")
+    public Response getSparql(@PathParam("query") String query) {
+        return Response.ok().entity("<xml>Success</xml>").build();
     }
 
   /*  static SharingHolder sharing = sharing().pathPrefix("/sparql").tag("sparql");
